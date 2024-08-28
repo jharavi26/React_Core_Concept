847fa10310d5f0851e19ddadab8fb3d403787358
Ravikumar Vishwanath Jha <120722758+jharavi26@users.noreply.github.com>
2024-08-28 02:27:32 -0700

    Add files via upload

---
+++ state-concept/src/components/index.jsx
@@ -0,0 +1,15 @@
+
+function ProductList({name, city}){
+    // const {name, city} = props;
+    
+    return(
+         <div>
+            {
+            <h3>This is {name},lives in {city} </h3> 
+            }
+            
+        </div>
+    );
+}
+
+export default ProductList;
