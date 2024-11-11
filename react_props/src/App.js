2b1a3e059a2939ac7b9faaaa0901afabefa0326c
Ravikumar Jha <jharavi153@gmail.com>
2024-11-11 20:56:48 +0530

    Props declared

--- react_props/src/App.js
+++ react_props/src/App.js
@@ -1,24 +1,14 @@
-import logo from './logo.svg';
+
 import './App.css';
+import Profile from './component/profile';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+    <h1>Hello Ravikumar Welcome Back After so long time</h1>
+    <Profile text ="Welcome Rajan"/>
+    </>
+   
   );
 }
 
---
+++ react_props/src/component/profile.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Profile(prop){
+    return (
+        <>
+        <h1>{prop.text}</h1>
+        </>
+    )
+}
+
+export default Profile;
