100e629391552e0e3c3d886ad6ee31565cb7596c
Ravikumar Jha <jharavi153@gmail.com>
2024-11-15 20:46:31 +0530

    correction

--- react_props/src/App.js
+++ react_props/src/App.js
@@ -2,15 +2,10 @@ import './App.css';
 import Header from './component/Header';
 
 function App() {
-  function handleClick()
-{
-      alert("I am clicked")
-}  
 return (
     <div>
       <h1>I am Parent component</h1>
-    <Header name="Ravikumar" age="25"/>
-    <button onClick={handleClick}>Click me </button>
+    <Header name="Ravikumar" age="25" city="mumbai"/>
     </div>
   );
 }
